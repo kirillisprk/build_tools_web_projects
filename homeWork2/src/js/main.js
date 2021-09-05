3c1c5057c53b5e936334f8de977354a1e7273066
kirill <kmeleshkin@mail.ru>
2021-09-05 18:13:42 +0700

    ДЗ №2 добавил больше медиа

--- homeWork2/src/js/main.js
+++ homeWork2/src/js/main.js
@@ -2,8 +2,8 @@ import './tabs/tabs.js';
 import './timer/timer.js';
 import './calculatorDate/calculatorDate.js';
 import '../style/main.scss';
-import '../notification.mp3';
-
+import '../index.html';
+import '../../node_modules/focus-visible/dist/focus-visible'
 
 
 
